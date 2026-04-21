82ae5ed770d0e2c73b5d00f4fde00f7f9e423da7
danir28 <d.r7428331@gmail.com>
2026-04-21 18:55:11 -0300

    ejercicios hasta el 9

--- ConsoleApp1/Unidad 6/unidad 6.cs
+++ ConsoleApp1/Unidad 6/unidad 6.cs
@@ -16,7 +16,7 @@ for (int i = 0; i < 5; i++)
     suma += numeros[i];
 }
 Console.WriteLine($"La suma de los números es: {suma} ");
-*/
+
 //Ejercicio 2
 List<int> calificaciones = new List<int>();
 int suma1 = 0;
@@ -34,30 +34,118 @@ promedio = suma1 / calificaciones.Count;
 Console.WriteLine($"El promedio de las calificaciones es: {promedio} ");
 
 //Ejercicio 3
-int BusArreglo = 23;
+int Buscado = 23;
 
 for (int i = 0; i < numeros.Length; i++)
 {
-    if (numeros[i] == BusArreglo)
+    if (numeros[i] == Buscado)
     {
-        Console.WriteLine($"El número {BusArreglo} se encuentra en el arreglo.");
+        Console.WriteLine($"El número {Buscado} se encuentra en el arreglo.");
         break;
     }
 }
 
-int BusLista = 9;
-for (int i = 0; i < calificaciones.Count; i++)
+calificaciones.IndexOf(23);
+if (calificaciones.Contains(Buscado))
 {
-    if (calificaciones[i] == BusLista)
-    {
-        Console.WriteLine($"El número {BusLista} se encuentra en la lista.");
-        break;
-    }
+    Console.WriteLine($"El número {Buscado} se encuentra en la lista.");
+}
+else
+{
+    Console.WriteLine($"El numero {Buscado} no se encuentre en la lista.");
 }
 
 //Ejericio 4
 List<int> numeros = new List<int>();
 for (int i = 0; i < 5; i++)
 {
-    
+    Console.WriteLine("Ingrese un número:");
+    int n = int.Parse(Console.ReadLine());
+    numeros.Add(n);
 }
+numeros.Sort();
+numeros.Reverse();
+foreach (int n in numeros)
+{
+    Console.WriteLine(n);
+}
+
+//Ejercicio 5
+int[] num = new int[5];
+for (int i = 0; i < num.Length; i++)
+{
+    Console.WriteLine("Ingrese un número:");
+    int n1 = int.Parse(Console.ReadLine());
+    num[i] = n1;
+}
+Console.WriteLine("Los numeros pares ingresados son: ");
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] % 2 == 0)
+    {
+        Console.WriteLine(num[i]);
+    }
+}
+
+//Ejercicio 6
+List<int> nuemros1 = new List<int>();
+Console.WriteLine("Ingrese un numero: ");
+int n2 = int.Parse(Console.ReadLine());
+while (n2 != 0)
+{
+    nuemros1.Add(n2);
+    Console.WriteLine("Ingrese un numero (si quiere salir ingrese 0): ");
+    n2 = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("ingrese el numero del que quiere conocer la cantidad: ");
+int infon2 = int.Parse(Console.ReadLine());
+int contador = 0;
+for (int i = 0; i < nuemros1.Count; i++)
+{
+    if (nuemros1[i] == infon2)
+    {
+        contador += 1;
+    }
+}
+Console.WriteLine($"El numero {infon2} se encuentra {contador} veces en la lista");
+
+//Ejercicio 7
+List<string> nombres = new List<string>();
+Console.WriteLine("Ingrese un nombre (si ingresas fin termina): ");
+string nomb = Console.ReadLine();
+while (nomb != "fin")
+{
+    nombres.Add(nomb);
+    Console.WriteLine("Ingrese un nombre (si ingresas fin termina): ");
+    nomb = Console.ReadLine();
+}
+
+//Ejercicio 8
+string[] nombres1 = new string[5];
+for (int i = 0; i < nombres1.Length; i++)
+{
+    Console.WriteLine("Ingrese un nombre: ");
+    string nomb1 = Console.ReadLine();
+    nombres1[i] = nomb1;
+}
+Array.Reverse(nombres1);
+foreach (string nomb1 in nombres1)
+{
+    Console.WriteLine(nomb1);
+}
+
+//Ejercicio 9
+List<int> numeros2 = new List<int>();
+Console.WriteLine("Ingrese un numero: ");
+int n3 = int.Parse(Console.ReadLine());
+while (n3 != 0)
+{
+    numeros2.Add(n3);
+    Console.WriteLine("Ingrese un numero (si quiere salir ingrese 0): ");
+    n3 = int.Parse(Console.ReadLine());
+}
+int max = numeros2.Max();
+int min = numeros2.Min();
+Console.WriteLine($"El numero mayor es: {max}, y el minimos es {min}");
+*/
+//Ejercicio 10
