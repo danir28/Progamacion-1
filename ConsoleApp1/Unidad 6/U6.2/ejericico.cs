8f1e82639e4e95154be249f9366839f0262866a9
danir28 <d.r7428331@gmail.com>
2026-04-23 19:56:18 -0300

    acomodamos carpetas de unidad 6, e hicimos nuevos ejercicios que se tienen que presentar antes del domingo

---
+++ ConsoleApp1/Unidad 6/U6.2/ejericico.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+/*
+//Ejercicio 1
+Console.WriteLine("califica el sevicio (1-5) 0 para salir: ");
+int calificacion =int.Parse(Console.ReadLine());
+int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0;
+int usuarios = 0;
+while (calificacion != 0)
+{
+    if (calificacion < 1 || calificacion > 5)
+    {
+        Console.WriteLine("Calificacion incorrecta, intente de nuevo");
+    }
+    else
+    {
+        if (calificacion == 1)
+        {
+            n1++;
+        }
+        else if (calificacion == 2)
+        {
+            n2++;
+        }
+        else if (calificacion == 3)
+        {
+            n3++;
+        }
+        else if (calificacion == 4)
+        {
+            n4++;
+        }
+        else if (calificacion == 5)
+        {
+            n5++;
+        }
+        usuarios++;
+    }
+    Console.WriteLine("califica el sevicio (1-5) 0 para salir: ");
+    calificacion = int.Parse(Console.ReadLine());
+}
+if (usuarios > 0)
+{
+    Console.WriteLine($"Calificacion 1: {(float)n1 / usuarios * 100}%");
+    Console.WriteLine($"Calificacion 2: {(float)n2 / usuarios * 100}%");
+    Console.WriteLine($"Calificacion 3: {(float)n3 / usuarios * 100}%");
+    Console.WriteLine($"Calificacion 4: {(float)n4 / usuarios * 100}%");
+    Console.WriteLine($"Calificacion 5: {(float)n5 / usuarios * 100}%");
+}
+else
+{
+    Console.WriteLine("No se registraron calificaciones");
+}
+
+//Ejercicio 2
+int CursoA = 0, CursoB = 0, CursoC = 0;
+Console.WriteLine("Ingrese en que curso queire estar (A, B, C) o X para salir: ");
+string opcion = Console.ReadLine().ToUpper();
+while (opcion != "X")
+{
+    if (opcion == "A") CursoA++;
+    else if (opcion == "B") CursoB++;
+    else if (opcion == "C") CursoC++;
+    else Console.WriteLine("Curso no valido, intente de nuevo");
+    Console.WriteLine("Ingrese en que curso queire estar (A, B, C) o X para salir: ");
+    opcion = Console.ReadLine().ToUpper();
+}
+Console.WriteLine($"Curso A: {CursoA} alumnos");
+Console.WriteLine($"Curso B: {CursoB} alumnos");
+Console.WriteLine($"Curso C: {CursoC} alumnos");
+*/
+//Ejercicio 3
